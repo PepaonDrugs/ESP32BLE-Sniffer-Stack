8d68f6843ad660e25a1434c75d0ff5e2aaa82827
Pepa on Drugs <93032314+PepaonDrugs@users.noreply.github.com>
2023-04-27 21:25:46 +0200

    Add files via upload

--- ESP32BLE-Sniffer.py
+++ ESP32BLE-Sniffer.py
@@ -14,6 +14,7 @@ influx_user = os.environ['INFLUX_USER']
 influx_pass = os.environ['INFLUX_PASS']
 influx_db = os.environ['INFLUX_DATABASE']
 
+
 # Create an InfluxDB client instance
 influx_client = InfluxDBClient(host=influx_host, port=influx_port, username=influx_user, password=influx_pass, database=influx_db)
 
@@ -21,54 +22,57 @@ influx_client = InfluxDBClient(host=influx_host, port=influx_port, username=infl
 def on_message(client, userdata, message):
     # Convert the message payload from bytes to string
     json_data = message.payload.decode('utf-8')
-    # Load the JSON data into a Python dictionary
-    data = json.loads(json_data)
-    # Count the number of devices in the JSON data
-    num_devices = len(data["Results"])
-    print("There are", num_devices, "devices around.")
-    # Extract the RSSI values for each device in the JSON data
-    rssi_values = []
-    for device in data["Results"]:
-        rssi = data["Results"][device].get("rssi")
-        if rssi is not None:
-            rssi_values.append(rssi)
-            print("RSSI value for device", device, ":", rssi)
-    # Calculate the highest, lowest, and average RSSI
-    if rssi_values:
-        highest_rssi = max(rssi_values)
-        lowest_rssi = min(rssi_values)
-        avg_rssi = sum(rssi_values) / len(rssi_values)
-        print("Highest RSSI:", highest_rssi)
-        print("Lowest RSSI:", lowest_rssi)
-        print("Average RSSI:", avg_rssi)
-        # Write the data to InfluxDB
-        data_points = [
-            {
-                "measurement": "device_count",
-                "fields": {
-                    "value": num_devices
+    try:
+        # Load the JSON data into a Python dictionary
+        data = json.loads(json_data)
+        # Count the number of devices in the JSON data
+        num_devices = len(data["Results"])
+        print("There are", num_devices, "devices around.")
+        # Extract the RSSI values for each device in the JSON data
+        rssi_values = []
+        for device in data["Results"]:
+            rssi = data["Results"][device].get("rssi")
+            if rssi is not None:
+                rssi_values.append(rssi)
+                print("RSSI value for device", device, ":", rssi)
+        # Calculate the highest, lowest, and average RSSI
+        if rssi_values:
+            highest_rssi = max(rssi_values)
+            lowest_rssi = min(rssi_values)
+            avg_rssi = sum(rssi_values) / len(rssi_values)
+            print("Highest RSSI:", highest_rssi)
+            print("Lowest RSSI:", lowest_rssi)
+            print("Average RSSI:", avg_rssi)
+            # Write the data to InfluxDB
+            data_points = [
+                {
+                    "measurement": "device_count",
+                    "fields": {
+                        "value": num_devices
+                    }
+                },
+                {
+                    "measurement": "rssi_min",
+                    "fields": {
+                        "value": lowest_rssi,
+                    }
+                },
+                {
+                    "measurement": "rssi_avg",
+                    "fields": {
+                        "value": avg_rssi,
+                    }
+                },
+                {
+                    "measurement": "rssi_max",
+                    "fields": {
+                        "value": highest_rssi,
+                    }
                 }
-            },
-            {
-                "measurement": "rssi_min",
-                "fields": {
-                    "value": lowest_rssi,
-                }
-            },
-            {
-                "measurement": "rssi_avg",
-                "fields": {
-                    "value": avg_rssi,
-                }
-            },
-            {
-                "measurement": "rssi_max",
-                "fields": {
-                    "value": highest_rssi,
-                }
-            }
-        ]
-        influx_client.write_points(data_points)
+            ]
+            influx_client.write_points(data_points)
+    except json.decoder.JSONDecodeError:
+        print("Invalid JSON data received:", json_data)
 
 # Create a new MQTT client instance
 client = mqtt.Client()
--- README.md
+++ README.md
@@ -5,3 +5,43 @@
 Esp32 sniffes for Ble --> Sends to Mqtt server ---> RSSI min/maxi/low 
 
 and device count ---> dockercontainer writes into influxdb
+
+
+##Getting Started
+
+Upload esp32sniffer.ino with Huge APP partition scheme
+
+edit username password...
+```yaml
+#define MQTT_server "brokerip"
+#define MQTT_port 1883
+#define MQTT_user "username"
+#define MQTT_pass "password"
+#define MQTT_name "Esp32"
+```
+and wifi credentials
+```yaml
+const char* ssid = "your wifi name"; // WIFIN-Name
+const char* ssidkey = "your wifi password"; // WiFi Key
+```
+
+Run Docker 
+
+replace the following variables
+
+```yaml
+docker run -d \
+  --name='snifferstack' \
+  -e HOST_CONTAINERNAME="snifferstack" \
+  -e 'TOPIC'='ble/report' \
+  -e 'MQTT_BROKER'='mqtt-broker' \
+  -e 'INFLUX_HOST'='influx-ip' \
+  -e 'INFLUX_PORT'='8086' \
+  -e 'INFLUX_USER'='username' \
+  -e 'INFLUX_PASS'='password' \
+  -e 'INFLUX_DATABASE'='database' \
+  pepaondrugs/esp32ble-sniffer-stack:latest
+  ```
+
+
+thanks to [ct-Open-Source](https://github.com/ct-Open-Source/esp32-bt-mqtt-scanner/blob/fb5163728d1f0f77dccba8ccc45dc972ff15c33b/Bluetooth-MQTT.ino) for the esp32 code
